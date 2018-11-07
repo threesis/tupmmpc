6595995a769305071bfec4be655251481493461a
ysa <domingoysabelle@gmail.com>
2018-11-07 15:23:40 +0800

    test uli -ysa

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -281,21 +281,13 @@
     <section id="contact">
       <div class="container">
         <div class="row">
-<<<<<<< HEAD
           <div class="col-md-12 col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">Contact Us</h2>
           </div>
-=======
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Contact Us</h2>
-          </div>
-        </div>
->>>>>>> e1848fb6636284cf30f016583a12e7d301ac93b7
         <div class="row">
           <div class="col-lg-12">
             <form id="contactForm" name="sentMessage" novalidate="novalidate">
               <div class="row">
-<<<<<<< HEAD
                   <div class="form-group">
                     <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name.">
                     <p class="help-block text-danger"></p>
@@ -311,33 +303,6 @@
                       <button id="sendMessageButton" class="btn btn-primary btn-l text-uppercase" type="submit">Send Message</button>
                   </div>
                 </div>
-=======
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name.">
-                    <p class="help-block text-danger"></p>
-                  </div>
-                  <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Email" required="required" data-validation-required-message="Please enter your email address.">
-                    <p class="help-block text-danger"></p>
-                  </div>
-                  <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Phone" required="required" data-validation-required-message="Please enter your phone number.">
-                    <p class="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <textarea class="form-control" id="message" placeholder="Text" required="required" data-validation-required-message="Please enter a message."></textarea>
-                    <p class="help-block text-danger"></p>
-                  </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-lg-12 text-center">
-                  <div id="success"></div>
-                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-                </div>
->>>>>>> e1848fb6636284cf30f016583a12e7d301ac93b7
               </div>
             </form>
           </div>
