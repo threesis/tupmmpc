c7ed26e90c4d8a81294b033a84f1f4707cc5c96e
ysa <domingoysabelle@gmail.com>
2018-11-07 15:16:56 +0800

    test ysa hehe

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -307,7 +307,7 @@
                             </form>
                             <div class="modal-footer">
                               <button type="submit" id="saveLoan" class="btn btn-primary">Apply</button>
-                              <button type="button" id="closeLoan" class="btn btn-secondary" data-dismiss="modal">CloseYSAAAAAA</button>
+                              <button type="button" id="closeLoan" class="btn btn-secondary" data-dismiss="modal">CloseYSsssAAAAAA</button>
                             </div>
                           </div>
                         </div>
